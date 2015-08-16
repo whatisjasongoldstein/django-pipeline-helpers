4da01667aa10c9fef83e1f98e7cf7c6dba989c69
Jason Goldstein <jason@betheshoe.com>
2015-08-16 14:55:15 -0400

    Shamelessly backport @ctbarna's namespacing idea. Do caching slightly differently (and in a way that actually works)

--- pipeline_helpers.py
+++ pipeline_helpers.py
@@ -1,27 +1,54 @@
 import importlib
 from django.conf import settings
 
+# Assets with be cached the first 
+# time module is imported.
+
 _js = {}
 _css = {}
-_cached = False
+cached = False
+
+
+def _add_namespace(namespace, data):
+    """
+    Add namespace:key to each key in data.
+    """
+    # modifying the dict as you iterate over it 
+    # would hit every key multiple times
+    keys = data.keys()  
+    
+    for key in keys:
+        data["%s:%s" % (namespace, key)] = data.pop(key)
 
 def _assets_from_apps(js=False, css=False):
-    if not _cached:
-        for app in settings.INSTALLED_APPS:
-            try:
-                assets = importlib.import_module("%s.assets"% app)
-            except ImportError:
-                continue
+    """
+    Search the INSTALLED_APPS for assets.py files,
+    and fill the js/css with its contents. Namespace
+    bundles with their app's name.
+    """
+    for app in settings.INSTALLED_APPS:
+        try:
+            assets = importlib.import_module("%s.assets"% app)
+            js = getattr(assets, "PIPELINE_JS", {})
+            css = getattr(assets, "PIPELINE_CSS", {})
+            _add_namespace(app, js)
+            _add_namespace(app, css)
             _js.update(getattr(assets, "PIPELINE_JS", {}))
             _css.update(getattr(assets, "PIPELINE_CSS", {}))
+        except ImportError:
+            continue
     if js:
         return _js
     if css:
         return _css
 
-def find_js():
-    return _assets_from_apps(js=True)
+if not cached:
+    _assets_from_apps()
+    cached = True
 
+
+def find_js():
+    return _js
 
 def find_css():
-    return _assets_from_apps(css=True)
+    return _css
